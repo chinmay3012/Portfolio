84f38a247a51129fcfd3ec3bedbc0785987cd6df
Chinmay Mehrotra <88617477+chinmay3012@users.noreply.github.com>
2025-05-31 23:53:09 +0530

    Update index.js
    changed the method from post to GET and so from req.body to req.query as GET method extract data from query string

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-function sending(){
-    const name1=document.getElementById("name").value;
-    const email1=document.getElementById("gmail").value;
-    const feedback1 = document.getElementById("feedbackk").value;
+// function sending(){
+//     const name1=document.getElementById("name").value;
+//     const email1=document.getElementById("gmail").value;
+//     const feedback1 = document.getElementById("feedbackk").value;
 
-    console.log(name1);
-    console.log(email1);
-    console.log(feedback1);
+//     console.log(name1);
+//     console.log(email1);
+//     console.log(feedback1);
 
-    event.preventDefault(); //prevents page reloads
-}
+//     event.preventDefault(); //prevents page reloads
+// }
 
 
 
-        // const express = require("express");
-        // const app=express();
-        // const PORT= 3000;
+        const express = require("express");
+        const app=express();
+        const PORT= 3000;
 
         
-        // app.use(express.urlencoded({extended:true}));
-        // const path = require("path");
-        // app.use(express.static(path.join(__dirname)));
+        app.use(express.urlencoded({extended:true}));
+        const path = require("path");
+        app.use(express.static(path.join(__dirname))); //__dirname is the node directory 
 
-        // const Datas=[];
+        const Datas=[];
         
-        // app.post('/submit' , (req,res)=>{
+        app.get('/submit' , (req,res)=>{
 
-        //     const{name,email,feedback } = req.body;
-        //     const dataObj={name , email , feedback};
-        //     Datas.push(dataObj);//pushing the object
+            const{name,email,feedback } = req.query;
+            const dataObj={name , email , feedback};
+            Datas.push(dataObj);//pushing the object
 
-        //     console.log("Datas Arrays:" , Datas);
+            console.log("Datas Arrays:" , Datas);
 
-        //     res.send("Form data stored successfully");
+            res.send("Form data stored successfully");
 
-        // })
+        })
 
-        // app.listen(3000);
+        app.listen(3000);
